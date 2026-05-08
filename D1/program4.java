8f7369981235e7c5e25af92d93cff4e4ac7a3974
Siddharamkore <siddharamkore18@email.com>
2026-05-08 04:41:08 +0000

    4th code added

---
+++ D1/program4.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class ArrayDemo{
+
+	public static void main(String [] args ){
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter size of an array :");
+		int n = sc.nextInt();
+
+		int arr[] = new int[n];
+	        System.out.println("Enter array elements:");
+		for(int i = 0 ; i < n ; i++){
+			
+			arr[i] = sc.nextInt();
+		}
+		System.out.print("Max Ele  of Array :");
+		int max = Integer.MIN_VALUE;
+
+		for(int i = 0 ; i < n ; i++){
+
+                        if(arr[i]>max) max =arr[i];
+                }	
+	
+		System.out.println(max);
+	}
+}
