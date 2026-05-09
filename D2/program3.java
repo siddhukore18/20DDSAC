e38960d759765f969fe85a21425c9b37fa5135f5
Siddharamkore <siddharamkore18@email.com>
2026-05-09 09:04:01 +0000

    abc

---
+++ D2/program3.java
@@ -0,0 +1,37 @@
+import java.util.*;
+class ArrayDemo{
+
+	public static void main(String [] args ){
+	
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter size of an array : ");
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		System.out.println("enter array elements :");
+		for(int i = 0 ; i < n ; i++){
+
+			arr[i] = sc.nextInt();
+		}
+
+		System.out.println("enter your element :");
+		int ele = sc.nextInt();
+		int index =0  ;
+		boolean flag = false ;
+		for(int i = 0 ; i < n ; i++ ){
+		
+			if(arr[i]==ele){
+			       	flag = true ;
+				index = i;
+				break ;
+			}
+
+		}
+
+		if(flag){
+
+			System.out.println(ele + " is present in array at : "+index );
+
+		}
+		else  System.out.println(ele + " is not present in array ");
+	}
+}
---
+++ D2/program5.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class ArrayDemo{
+
+	public static void main(String [] args ){
+	
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter size of an array : ");
+		int n = sc.nextInt();
+		int arr[] = new int[n];
+		System.out.println("enter array elements :");
+		for(int i = 0 ; i < n ; i++){
+
+			arr[i] = sc.nextInt();
+		}
+
+		System.out.println("enter your element :");
+		int ele = sc.nextInt();
+		int count =0;
+		boolean flag = false ;
+		for(int i = 0 ; i < n ; i++ ){
+		
+			if(arr[i]==ele){
+			       	count ++ ;
+				
+			}
+
+		}
+
+		if(count == 1){
+
+			System.out.println(ele + " is unique");
+
+		}
+		else  System.out.println(ele + " is not not in array ");
+	}
+}
