9820316cc858457a053b4be950934f2da8ecd0ff
Siddharamkore <siddharamkore18@gmail.com>
2026-05-10 08:20:40 +0000

    1st code complated

---
+++ D3/program32.java
@@ -0,0 +1,53 @@
+import java.util.*;
+class ArrayDemo{
+
+	public static void main(String [] args ){
+	
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter size of an array 1: ");
+		int n1 = sc.nextInt();
+		int arr1[] = new int[n1];
+		System.out.println("enter array elements :");
+		for(int i = 0 ; i < n1 ; i++){
+
+			arr1[i] = sc.nextInt();
+		}
+
+		
+		System.out.println("Enter size of an array 2: ");
+                int n2 = sc.nextInt();
+                int arr2[] = new int[n2];
+                System.out.println("enter array elements :");
+                for(int i = 0 ; i < n2 ; i++){
+
+                        arr2[i] = sc.nextInt();
+                }
+
+		
+
+
+		boolean flag = true ;
+
+		if(arr1.length!=arr2.length) flag = false ;
+		else{
+			for(int i=0;i<arr1.length;i++){
+
+				if(arr1[i]!=arr2[i]) {
+				
+					flag = false ;
+					break;
+				}
+			}
+		}
+
+		if(flag){
+		
+			System.out.println("array1 and array2 are same in order and elements");
+		}else{
+		
+			System.out.println("array1 and array2 are not same in order or elements");
+		}
+
+
+	}
+}
