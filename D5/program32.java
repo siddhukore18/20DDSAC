e659c615480e1234cd59a1efb97f6f2a0d9622fc
Siddharamkore <siddharamkore18@gmail.com>
2026-05-12 10:11:08 +0000

    32 code added

--- D5/program32.java
+++ D5/program32.java
@@ -11,7 +11,7 @@ class StringDemo{
 		String str = sc.nextLine();
 		String rstr = "";
 
-                for(int i = str.length() -1; i>=0 ; i--){
+                for(int i = 0; i<str.length() ; i++){
 		
 			if(str.charAt(i)==' ' ) {
 				continue;
