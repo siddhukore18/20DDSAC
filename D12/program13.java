a6ea2cc2088643656b29068c6e376e6ae122cde9
Siddharamkore <siddharamkore18@gmail.com>
2026-05-19 10:48:47 +0000

    977

---
+++ D12/program13.java
@@ -0,0 +1,14 @@
+//leetcode  977
+
+
+
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        for(int i =0;i<nums.length;i++){
+            nums[i]=nums[i]*nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+
+    }
+}
