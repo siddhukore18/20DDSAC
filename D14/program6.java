4649dd0178323e931646f761ff9510efeea70ca8
Siddharamkore <siddharamkore18@gmail.com>
2026-05-21 04:04:19 +0000

    49

---
+++ D14/program6.java
@@ -0,0 +1,40 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        char arr[][] = new char[strs.length][];
+
+        for(int i = 0; i < strs.length; i++) {
+
+            arr[i] = strs[i].toCharArray();
+
+            Arrays.sort(arr[i]);
+        }
+
+        List<List<String>> rstr = new ArrayList<>();
+
+        for(int i = 0; i < strs.length; i++) {
+
+            if(arr[i] == null) continue;
+
+            char temp[] = arr[i];
+
+            List<String> larr = new ArrayList<>();
+
+            for(int j = i; j < strs.length; j++) {
+
+                if(arr[j] == null) continue;
+
+                if(Arrays.equals(temp, arr[j])) {
+
+                    larr.add(strs[j]);
+
+                    arr[j] = null;
+                }
+            }
+
+            rstr.add(larr);
+        }
+
+        return rstr;
+    }
+}
