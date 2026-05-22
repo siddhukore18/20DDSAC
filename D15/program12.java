3f8a09133790159e506f56108186b8de3868345a
Siddharamkore <siddharamkore18@gmail.com>
2026-05-22 14:58:37 +0000

    229

---
+++ D15/program12.java
@@ -0,0 +1,27 @@
+//leetcode 229
+
+class Solution {
+    public List<Integer> majorityElement(int[] nums) {
+
+
+        HashMap<Integer , Integer> map = new HashMap<>();
+
+        for(int i =0;i<nums.length;i++){
+            if(map.get(nums[i])==null ) map.put(nums[i],1);
+            else map.put(nums[i],map.get(nums[i])+1);
+        }
+
+        List<Integer>  list = new ArrayList<>();
+
+
+        for(Map.Entry<Integer, Integer> e : map.entrySet()){
+
+            if(e.getValue()*3>nums.length){
+                list.add(e.getKey());
+            }
+        }
+        return list;
+
+        
+    }
+}
