b688219f79bd97146f453ee3b63d764c193fcc17
Siddharamkore <siddharamkore18@gmail.com>
2026-05-30 03:05:40 +0000

    121

--- D18/leetcode121.java
+++ D18/leetcode121.java
@@ -4,13 +4,16 @@
 
 class Solution {
     public int maxProfit(int[] prices) {
-        int max = 0;
+        int max = 0,minP=prices[0];;
+
 
 
         for(int i = 0 ; i< prices.length; i++){
-            for(int j = i+1;j<prices.length;j++){
-                max=Math.max(prices[j]-prices[i],max);
-            }
+
+            if(minP>prices[i]) minP= prices[i];
+            
+            max=Math.max(prices[i]-minP,max);
+            
         }
         return max;
     }
