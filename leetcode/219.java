4a8bb0c184b0889eaf5e073850dace256c81db9f
Siddharamkore <siddharamkore18@gmail.com>
2026-06-01 02:48:12 +0000

    219

---
+++ leetcode/219.java
@@ -0,0 +1,14 @@
+///  leetcode 219
+
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+        for(int i = 0 ; i< nums.length ;i++){
+            for(int j =i+1;j<nums.length&&j<i+k+1;j++){
+                if(nums[i]==nums[j]&&Math.abs(i-j)<=k)return true;
+            }
+        }
+        return false;
+    }
+}
