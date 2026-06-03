e6e27e2f370af3cb17dc19f20678973914fd698f
Siddharamkore <siddharamkore18@gmail.com>
2026-06-03 10:39:02 +0000

    260

---
+++ leetcode/260.java
@@ -0,0 +1,20 @@
+//leetcode 260
+
+
+
+class Solution {
+    public int[] singleNumber(int[] nums) {
+
+        Set<Integer>  set = new HashSet<>();
+        for(int i =0; i<nums.length;i++){
+            if(set.contains(nums[i])) set.remove(nums[i]);
+            else  set.add(nums[i]);
+        }
+
+        return set.stream()
+               .mapToInt(Integer::intValue)
+               .toArray();
+
+
+    }
+}
