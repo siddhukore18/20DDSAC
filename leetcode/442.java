5139a42b7d28872429a87cdd8669d95e26bf00e4
Siddharamkore <siddharamkore18@gmail.com>
2026-06-05 12:14:28 +0000

    442

---
+++ leetcode/442.java
@@ -0,0 +1,21 @@
+///leetcode 442
+import java.utils.*;
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+
+        Set<Integer>  set = new HashSet<>();
+
+        List<Integer>  result = new ArrayList<>();
+
+        for(int i = 0 ; i < nums.length ;i++){
+            if(set.contains(nums[i])){
+                result.add(nums[i]);
+            }else{
+                set.add(nums[i]);
+            }
+        }
+
+        return result;
+
+    }
+}
