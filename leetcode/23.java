3bd09a8c86fb12a0a7f73c2a57019488b7abcc34
Siddharamkore <siddharamkore18@gmail.com>
2026-06-09 05:43:30 +0000

    23a

--- leetcode/23.java
+++ leetcode/23.java
@@ -1,18 +1,19 @@
 ///leetcode 23
 
-
+import java.util.*;
 
 
 /**
  * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
  */
+class ListNode {
+       int val;
+       ListNode next;
+       ListNode() {}
+       ListNode(int val) { this.val = val; }
+       ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+ 
 class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         PriorityQueue<ListNode> pq = new PriorityQueue<>(
