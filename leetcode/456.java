5e5cb6d04792825d2d6c7e8c4eaeb79f5b317c45
Siddharamkore <siddharamkore18@gmail.com>
2026-06-09 18:34:59 +0000

    456

---
+++ leetcode/456.java
@@ -0,0 +1,23 @@
+//leetcode 456
+
+
+class Solution {
+    public boolean find132pattern(int[] nums) {
+        Stack<Integer> stack = new Stack<>();
+        int third = Integer.MIN_VALUE;
+
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] < third) {
+                return true;
+            }
+
+            while (!stack.isEmpty() && nums[i] > stack.peek()) {
+                third = stack.pop();
+            }
+
+            stack.push(nums[i]);
+        }
+
+        return false;
+    }
+}
